0951c48ac4e91041801dc01466b75cfa7e423778
Felipe <machadofelipe2016@outlook.com>
2022-03-20 13:34:29 -0300

    c6- adding other information about index

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express= require('express')
 const api = express()
 const port = process.env.PORT || 3000
+const path = require('path')
 
+const session = require('express-session')
+const UserModel = require('./models/user')
 //create variable to receive bodyparser
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser')
 
 
 // create connect mongo and mongoose
@@ -12,15 +15,60 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 
+
+api.use(session({
+    secret:'teste',
+    resave: true,
+    saveUninitialized: true
+}))
+
+
+api.set('views', path.join(__dirname, 'views'))
+api.set('view engine','ejs')
+api.use(express.static(path.join( __dirname ,'views')))
+
+// connection about body parser
+api.use(bodyParser.urlencoded({extended:true}))
+
 api.get('/', (req, res) => {
-    res.send('let working and learning more and more')
+    res.send('allows learnig moreevry single day')
 })
+
+api.post('/login', async(req,res) => {
+    const user = await UserModel.findOne({username: req.body.username}) 
+    const isValid = await user?.checkPassword(req.body.password)
+      
+    if(isValid){
+      req.session.user = user
+      res.redirect('/')
+    }else{
+        res.redirect('/login')
+   }    
+})
+
+const CreateInitialuser =  async() => {
+    const total = await UserModel.count({username:'felipe martins'})
+    if(total === 0){
+        const user = new UserModel ({
+            username:'felipe martins',
+            email:'machadofelipe2016@outlook.com',
+            password:'martins18',
+        })
+       await user.save()
+       console.log('user created')
+    }else{
+        console.log('user created shiped')
+    }
+}
+
+
 
 mongoose.connect(mongodb, {useNewUrlParser:true,
     useUnifiedTopology: true
 })
 .then(() => {
-    api.listen(port, () => console.log('listing of port: ', port))
+    CreateInitialuser()
+    api.listen(port, () => console.log('listing of port: '+ port))
 })
 .catch(e => console.log(e))
 
